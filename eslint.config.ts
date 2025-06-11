b178319c071d41afc1cd126f12e2bf6ed0a86b0f
Franci Penov <francip@gmail.com>
2025-06-10 18:22:10 -0700

    Eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,67 +1,67 @@
-import js from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import * as importPlugin from 'eslint-plugin-import'
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import importPlugin from "eslint-plugin-import";
 
-import globals from 'globals'
+import globals from "globals";
 
 export default tseslint.config(
     {
         ignores: [
-            '**/*.test.{js,ts}',
-            '**/*.spec.{js,ts}',
-            'node_modules/**',
-            'build/**',
-            'dist/**',
-            'dev-dist/**',
-            'coverage/**',
-        ]
+            "**/*.test.{js,ts}",
+            "**/*.spec.{js,ts}",
+            "node_modules/**",
+            "build/**",
+            "dist/**",
+            "dev-dist/**",
+            "coverage/**",
+        ],
     },
     {
-        files: ['**/*.js'],
+        files: ["**/*.js"],
         extends: [js.configs.recommended],
         languageOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module',
-            globals: {...globals.browser, ...globals.node}
-        }
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: { ...globals.browser, ...globals.node },
+        },
     },
     {
-        files: ['**/*.ts'],
-        extends: [
-            js.configs.recommended,
-            ...tseslint.configs.recommended,
-        ],
+        files: ["**/*.ts"],
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
         plugins: {
-            import: importPlugin
+            import: importPlugin,
         },
         settings: {
-            'import/resolver': {
+            "import/resolver": {
                 typescript: {
-                    project: ['./tsconfig.{app,node}.json'],
+                    project: ["./tsconfig.{app,node}.json"],
                 },
             },
         },
         languageOptions: {
-            ecmaVersion: 'latest',
-            sourceType: 'module',
-            globals: {...globals.browser, ...globals.node},
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: { ...globals.browser, ...globals.node },
             parser: tseslint.parser,
             parserOptions: {
-                project: ['./tsconfig.{app,node}.json'],
-                tsconfigRootDir: '.',
+                project: ["./tsconfig.{app,node}.json"],
+                tsconfigRootDir: ".",
             },
         },
         linterOptions: {
             reportUnusedDisableDirectives: true,
         },
         rules: {
-            '@typescript-eslint/no-explicit-any': 'error',
-            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-            '@typescript-eslint/consistent-type-imports': [
-                'error',
-                { prefer: 'type-imports' },
+            "@typescript-eslint/no-explicit-any": "error",
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                { argsIgnorePattern: "^_" },
             ],
-            '@typescript-eslint/no-import-type-side-effects': 'error',
+            "@typescript-eslint/consistent-type-imports": [
+                "error",
+                { prefer: "type-imports" },
+            ],
+            "@typescript-eslint/no-import-type-side-effects": "error",
         },
-    },
-)
+    }
+);
